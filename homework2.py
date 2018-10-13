cde5c82ffc110440fefc2a77b21f0dc7d229b96d
Bram van Dooremaal <bram@dooremaal.nl>
2018-10-13 03:43:21 +0200

    Linear Regression with Gradient Descent using MSE

--- homework2.py
+++ homework2.py
@@ -26,40 +26,50 @@ print("Transformed data")
 print(trans_data)
 print("")
 
+# Put the linear regression in a function def since i needed it multiple times during construction.
+def linear_regression(X, y, m_current=0, b_current=0, epochs=10, learning_rate=0.01):
+     N = float(len(y))
+     m_hist = np.zeros((epochs,2));
+     b_hist = np.zeros((epochs,2));
+     c_hist = np.zeros(epochs);
+     for i in range(epochs):
+          y_current = (m_current * X) + b_current
+          cost = sum([data**2 for data in (y-y_current)]) / N
+          m_gradient = -(2/N) * sum(X * (y - y_current))
+          b_gradient = -(2/N) * sum(y - y_current)
+          m_current = m_current - (learning_rate * m_gradient)
+          b_current = b_current - (learning_rate * b_gradient)
 
+          # Storing values of this iterations
+          m_hist[i,:] = m_current
+          b_hist[i,:] = b_current
+          c_hist[i] = cost
+     return m_hist, b_hist, c_hist
 
+# Values
+lr = 0.01;
+n_iter = 2000;
 
+m_hist,b_hist,c_hist = linear_regression(trans_data.X, trans_data.Y, 0, 0, n_iter, lr);
 
-#data
-#x = [26.37, 26.58,15.25,21.24,24.98]
-#y = [451.78,439.46,467.23,459.81,447.15]
+# A big figure containing all our plots
+fig = plt.figure(figsize=(5,4),dpi=200)
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
 
-#scaling
-#x_t = [(x_i - min(x)) / (max(x) - min(x)) for x_i in x]
-#print(minmax_x)
-#y_t = [(y_i - min(y)) / (max(y) - min(y)) for y_i in y]
-#print(minmax_y)
+# The regression lines plotted
+shown = [0,1,2,n_iter-1];
+count = 0;
+for i in shown:
+    count += 1;
+    ax = fig.add_subplot(4, 2, count);
+    ax.set_xlim(0,1);
+    ax.plot(trans_data.X, trans_data.Y,'b.');
+    x_vals = np.array(ax.get_xlim());
+    y_vals = b_hist[i] + m_hist[i] * x_vals;
+    ax.plot(x_vals, y_vals,'r-');
 
-#plt.scatter(x_t,y_t)
-#plt.show()
+# Adding the cost graph
+ax1 = fig.add_subplot(4, 2, count+1);
+ax1.plot(range(n_iter),c_hist,'b.')
 
-#SSE
-#transform X to matrix
-#m_x = np.matrix(x_t)
-#m_y = np.matrix(y_t)
-#x_mean = sum(x_t)/len(x_t)
-#y_mean = sum(y_t)/len(y_t)
-
-#x_var = 0
-#y_var = 0
-
-#for x in x_t:
-#    x_var = x_var + ((x-x_mean)**2)
-#for y in y_t:
-#    y_var = y_var + ((y-y_mean)**2)
-
-# cov = 0
-# for i in range(x_t):
-#     cov = cov + ((x_t[i] - x_mean) * (y_t[i] - y_mean))
-#     print(cov)
-
+plt.show();
